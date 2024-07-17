e649ce586c508a47b27153ef526b51b36279eef2
Teja <88682769+lammu21@users.noreply.github.com>
2024-07-17 06:30:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Number of occurrence/number-of-occurrence.java
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int x = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().count(arr, n, x);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    int count(int[] arr, int n, int x) {
+        // code here
+        int first=first(arr,x);
+        if(first==-1){
+            //  int [] result={-1,-1};
+            return 0;
+        }
+        int last=last(arr,x);
+        
+        return last-first+1;
+        
+    }
+    
+     public int first(int[] nums, int target) {
+        int len=nums.length;
+        int start=0;
+        int end=len-1;
+        int first=-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(nums[mid]==target){
+                first=mid;
+                end=mid-1;
+            }
+            else if(nums[mid]>target){
+                end=mid-1;
+            }
+            else {
+                start=mid+1;
+            }
+        }
+        return first;
+    }
+
+    public int last(int[] nums, int target) {
+        int len=nums.length;
+        int start=0;
+        int end=len-1;
+        int last=-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(nums[mid]==target){
+                last=mid;
+                start=mid+1;
+            }
+            else if(nums[mid]>target){
+                end=mid-1;
+            }
+            else {
+                start=mid+1;
+            }
+        }
+        return last;
+    }
+}
