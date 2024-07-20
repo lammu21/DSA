facdcf89c077d688b4dd68c022441b254bd05d80
Teja <88682769+lammu21@users.noreply.github.com>
2024-07-20 06:47:23 +0530

    Time: 0 ms (100%), Space: 49.8 MB (91.86%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+//  Edge case to check if array with one element 
+        if(nums.length==1){
+            return nums[0];
+        }
+// Check if first element is single element
+        if(nums[0]!=nums[1]){
+            return nums[0];
+        }
+
+// Check if Last element is single element 
+        if(nums[(nums.length)-1]!=nums[(nums.length)-2]){
+            return nums[(nums.length)-1];
+        }
+
+// Binary Search implementation starts here 
+
+int start =1,end=nums.length-2;
+
+      while(start<=end){
+        int mid = start+(end-start)/2;
+
+// check for mid element if it is single 
+
+        if(nums[mid]!=nums[mid-1]&&nums[mid]!=nums[mid+1]){
+             return nums[mid];
+        }
+// triming the search space by half 
+  
+
+       if(mid%2==0&&nums[mid]==nums[mid+1]||mid%2!=0&&nums[mid]==nums[mid-1]){
+          start=mid+1;
+       }
+       else {
+            end=mid-1;
+       }
+
+      }
+      return -1;
+   }
+}
