6cf143ae73a1038b7a4f3bb79c18bf62ff7934d8
Teja <88682769+lammu21@users.noreply.github.com>
2024-07-22 10:51:49 +0530

    Time: 0 ms (100%), Space: 45.9 MB (40.86%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,13 +1,16 @@
 class Solution {
-
-    public int first(int[] nums, int target) {
+// Regular binary search modified to search first occurence
+// Read comments to  understand changes 
+public int first(int[] nums, int target) {
         int len=nums.length;
         int start=0;
         int end=len-1;
+// variable to keep track of First position of Element in Sorted array 
         int first=-1;
         while(start<=end){
             int mid=(start+end)/2;
             if(nums[mid]==target){
+// If the element is = target we save in First variable and, we discard second or right half search of array ,i.e we are looking for first occurence that will be in left present as it is a sorted 
                 first=mid;
                 end=mid-1;
             }
@@ -21,13 +24,19 @@ class Solution {
         return first;
     }
 
+// Regular binary search modified to search Last occurence
+// Read comments to  understand changes 
+
     public int last(int[] nums, int target) {
         int len=nums.length;
         int start=0;
         int end=len-1;
+// variable to keep track of Last position of Element in Sorted array 
         int last=-1;
         while(start<=end){
             int mid=(start+end)/2;
+
+// If the element is = target we save in last variable and , we discard first or left half ,i.e we are looking for last occurence that will be in present right half  as it is a sorted 
             if(nums[mid]==target){
                 last=mid;
                 start=mid+1;
@@ -42,9 +51,12 @@ class Solution {
         return last;
     }
 
+// Leetcode function which calls first and second position's binary search
+
     public int[] searchRange(int[] nums, int target) {
 
         int first=first(nums,target);
+// if we dont find occurrence of number, i.e., array does've that number we can directly {-1,-1}
         if(first==-1){
              int [] result={-1,-1};
             return result;
