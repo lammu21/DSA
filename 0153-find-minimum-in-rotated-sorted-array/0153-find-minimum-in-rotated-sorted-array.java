6e41a7ca05cb327db6400ee95e8f287c8b253889
Teja <88682769+lammu21@users.noreply.github.com>
2024-07-26 07:02:18 +0530

    Time: 0 ms (100%), Space: 41.6 MB (79.69%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,23 +1,28 @@
 class Solution {
     public int findMin(int[] nums) {
-        int start =0;
-        int end=nums.length-1;
+        int start = 0;
+        int end = nums.length - 1;
 
-// min variable to store smallest element in array     
+        // min variable to store smallest element in array
         int min = Integer.MAX_VALUE;
 
-        while(start<=end){
-            int mid=start+(end-start)/2;
-// below if is check whether first half (start to mid ) is sorted or not 
-            if(nums[start]<=nums[mid]){
-// if first half sorted we pick start and compare it with min, if start is smaller we consider it as smallest until now, and we discard first half, because if first half is sorted start will be the minimum, we dont have to check for smallest in sorted first half
-                min =Math.min(nums[start],min);
-                start=mid+1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            // below if is check whether first half (start to mid ) is sorted or not
+            if (nums[start] <= nums[mid]) {
+                // if first half sorted we pick start and compare it with min, if start is
+                // smaller we consider it as smallest until now, and we discard first half,
+                // because if first half is sorted start will be the minimum, we dont have to
+                // check for smallest in sorted first half
+                min = Math.min(nums[start], min);
+                start = mid + 1;
             }
-// else second half sorted, we pick mid and compare it with min, if mid is smaller we consider it as smallest until now, we discard second half, because there no point to check for smallest in sorted, mid will be smallest 
+            // else second half sorted, we pick mid and compare it with min, if mid is
+            // smaller we consider it as smallest until now, we discard second half, because
+            // there no point to check for smallest in sorted, mid will be smallest
             else {
-                min=Math.min(nums[mid],min);
-                end=mid-1;
+                min = Math.min(nums[mid], min);
+                end = mid - 1;
             }
         }
         return min;
