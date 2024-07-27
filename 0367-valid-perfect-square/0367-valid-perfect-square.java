f9c1dc97ec9c3be9409d25a05da6ab0064f59be0
Teja <88682769+lammu21@users.noreply.github.com>
2024-07-27 06:39:41 +0530

    Time: 0 ms (100%), Space: 40.2 MB (42.81%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,28 @@
+class Solution {
+    // binary search with check condition
+    public boolean isPerfectSquare(int num) {
+
+        long start = 1;
+        long end = num;
+
+        while (start <= end) {
+
+            long mid = start + (end - start) / 2;
+            // if mid*mid equals to given num we'll return true
+            if (mid * mid == num) {
+                return true;
+            }
+            // check if mid*mid is less than num , we'll trim down left half, cause elements
+            // square will be lesser than num
+            else if (mid * mid < num) {
+                start = mid + 1;
+            }
+            // if mid*mid is not less than or equals to num, then it will be greater than
+            // num , triming right half, cause elements square will be greater than num
+            else {
+                end = mid - 1;
+            }
+        }
+        return false;
+    }
+}
