4027eb78fe9671807c1ba7e511ddd1c0eb242157
Teja <88682769+lammu21@users.noreply.github.com>
2024-08-01 07:06:27 +0530

    Time: 180 ms (5.18%), Space: 47 MB (99.69%) - LeetHub

--- 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.java
@@ -1,41 +1,38 @@
 class Solution {
     public int smallestDivisor(int[] nums, int threshold) {
 
-        int n=nums.length;
-       
-         
-         int start=1;
-         int max=Integer.MIN_VALUE;
-          n=nums.length;
-         for(int i =0;i<n;i++){
-            max=Math.max(max, nums[i]);
+        int n = nums.length;
+        int start = 1;
+        int max = Integer.MIN_VALUE;
+        n = nums.length;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, nums[i]);
         }
-        
-       
-         int end = max;
-         int result=-1;
-         while(start<=end){
-            int mid= start+(end-start)/2;
-            int sum=  sumOfDivisorByMid(nums,mid);
-           
-            if(sum<=threshold){
-                result =mid;
-               end=mid-1;
+        int end = max;
+        int result = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int sum = sumOfDivisorByMid(nums, mid);
+
+            if (sum <= threshold) {
+                result = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
-            else {
-                start=mid+1;
-            }
-         }
-         return result;
-        
+        }
+        return result;
+
     }
-    public static int sumOfDivisorByMid(int[]nums, int mid){
-        int sum=0;
-        int n=nums.length;
-        for(int i=0;i<n;i++){
-           sum += Math.ceil((double)(nums[i]) / (double)(mid));
-            }
-        
+
+    // Function to find sum of all remider values when divided by mid
+    public static int sumOfDivisorByMid(int[] nums, int mid) {
+        int sum = 0;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            sum += Math.ceil((double) (nums[i]) / (double) (mid));
+        }
+
         return sum;
     }
 }
