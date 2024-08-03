6f6b6e924c5a86e2b37ee9fa849dddd32bad302f
Teja <88682769+lammu21@users.noreply.github.com>
2024-08-03 20:00:46 +0530

    Time: 11 ms (74.82%), Space: 47.1 MB (40.48%) - LeetHub

---
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        // why we take max of array start , i.e.,we want make sure that we can carry any
+        // weight in within a day, if we take of max of array, we can carry all other
+        // weights in within a day
+        int start = Integer.MIN_VALUE;
+        // why we take summation of array as end i.e., summation is enough to carry ship
+        // all in a day, more summation can also do to optimize the we take summation
+        int end = 0;
+        for (int i = 0; i < weights.length; i++) {
+            start = Math.max(start, weights[i]);
+            end += weights[i];
+        }
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int daysReqToMid = calculateDaysReqToMid(weights, mid);
+            if (daysReqToMid <= days) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return start;
+    }
+    public int calculateDaysReqToMid(int[] weights, int capacity){
+        int days=1 ,load=0;
+          for (int i = 0; i < weights.length; i++) {
+            if(load+weights[i]>capacity){
+                days++;
+                load=weights[i];
+            }
+            else{
+                load+=weights[i];
+            }
+          }
+          return days;
+    }
+}
