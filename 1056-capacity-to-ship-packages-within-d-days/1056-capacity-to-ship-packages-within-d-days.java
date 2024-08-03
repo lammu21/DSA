af8fb7f6dbb5392c5ae1ed4799a018441dee43be
Teja <88682769+lammu21@users.noreply.github.com>
2024-08-03 22:29:46 +0530

    Time: 11 ms (74.82%), Space: 47 MB (65.28%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -42,3 +42,38 @@ class Solution {
           return days;
     }
 }
+
+//  most optimal code on leetcode
+// class Solution {
+//     // its not my solution
+//     public static boolean findDays(int[] a, int m, int d) {
+
+//         int cnt = 1, sum = 0;
+//         for (int w : a) {
+//             if (w > m)
+//                 return false;
+//             if ((sum += w) > m) {
+//                 if (++cnt > d)
+//                     return false;
+//                 sum = w;
+//             }
+//         }
+//         return true;
+//     }
+
+//     public int shipWithinDays(int[] weights, int days) {
+
+//         if (weights.length == 5 && weights[0] == 500 && weights[weights.length - 1] == 500) {
+//             return 1000;
+//         }
+//         int l = 0, r = 500 * weights.length / days;
+//         while (l < r) {
+//             int m = l + (r - l) / 2;
+//             if (findDays(weights, m, days))
+//                 r = m;
+//             else
+//                 l = m + 1;
+//         }
+//         return l;
+//     }
+// }
