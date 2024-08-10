ac6240ef868e84e925124267d1e2138a39112b6f
Teja <88682769+lammu21@users.noreply.github.com>
2024-08-10 12:23:49 +0530

    Time: 1 ms (59.56%), Space: 41.2 MB (48.49%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        int n=nums.length;
+        // if(k>n){
+        //     return -1;
+        // }
+        int start=Integer.MIN_VALUE;
+        int end=0;
+        
+        for(int i=0;i<n;i++){
+            if(start<nums[i]){
+                start=nums[i];
+               
+            }
+             end+=nums[i];
+        }
+        System.out.println(end);
+        if(k==1){
+            return end;
+        }
+        while(start<=end){
+            int mid = start+(end-start)/2;
+             int numOfSubarrays=findNumOfSubarrays(nums,mid);
+             
+             if(numOfSubarrays<=k){
+                end=mid-1;
+             }
+             else{
+                start=mid+1;
+             }
+
+        }
+        return start;
+    }
+    public static int findNumOfSubarrays(int [] nums, int sumLimit){
+        int numOfSubarrays=1;
+        int sumOfsubarray=0;
+        int n= nums.length;
+        for(int i=0;i<n;i++){
+            if(sumOfsubarray+nums[i]<=sumLimit){
+                sumOfsubarray+=nums[i];
+            }
+            else {
+                numOfSubarrays++;
+                sumOfsubarray=nums[i];
+            }
+        }
+        return numOfSubarrays;
+    }
+}
