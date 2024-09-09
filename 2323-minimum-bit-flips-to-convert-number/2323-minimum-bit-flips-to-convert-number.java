44b4d9d588c4ab709ea64c0ae0911b2793ac60d9
Teja <88682769+lammu21@users.noreply.github.com>
2024-09-09 09:08:01 +0530

    Need to revised again 2323-minimum-bit-flips-to-convert-number.java

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
@@ -5,7 +5,7 @@ class Solution {
         //  XOR of start and goal will give number of different bit in start and goal, 
         int numberOfDifferentBits=start^goal;
 
-        // below for loop will count number bit to be flipped 
+        // below for loop will count number bit to be flippedd
         int count=0;
         for(int i=0;i<31;i++){
             if((numberOfDifferentBits&(1<<i))!=0){
