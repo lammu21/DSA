867cea56e54854155945ece9c883f69775d9e428
Teja <88682769+lammu21@users.noreply.github.com>
2024-09-09 08:55:34 +0530

    Revised and commented 0231-power-of-two.java

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,12 +1,9 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
+        // Edge Case 
         if(n==0||n==-2147483648){
             return false;
         }
-           
-        // if(n==-2147483648){
-        //   return false;
-        // }
         if((n&n-1)==0){
             return true;
         }
