f7b9c64c6faac4415732f10f78d9f17999b14347
Teja <88682769+lammu21@users.noreply.github.com>
2024-10-10 07:09:55 +0530

    Time: 1 ms (98.48%), Space: 41.7 MB (24.74%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -7,13 +7,17 @@ class Solution {
         {
             if(ch=='(' || ch=='{' ||ch=='[')
             {
+                // if opening brackets should be stored in st 
                st.push(ch);
             }
             else
             {
+                // if closing brackets we should check in stack and first we have to if stack is empty if yes return flase
               if(st.isEmpty()){
                 return false;
               }
+
+            //   if stack is not empty pick top bracket and compare it with ch (closing brackt ) if open and closing matches continue 
               char cha=st.pop();
               if((ch==')' && cha=='(')||
                 (ch=='}' && cha=='{')||
@@ -21,11 +25,13 @@ class Solution {
                     continue;
                 }
                 else{
+                    // else return flase
                     return false;
                 }
             }
 
         }
+        // at last if stack is empty, return true
         return st.isEmpty();
 
     }
