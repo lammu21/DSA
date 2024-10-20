bc996f08c819ea21e813989f0041401ed3b91f57
Teja <88682769+lammu21@users.noreply.github.com>
2024-10-20 08:31:43 +0530

    Time: 5 ms (87.35%), Space: 44.2 MB (79.35%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -10,15 +10,18 @@ class Solution {
 
             // checking key is already presented in map
             if (mpp.containsKey(s.charAt(right))) {
+
                 // if present checking if is in current window, i.e., between left and right
                 if (mpp.get(s.charAt(right)) >= left) {
+
                     // if it is within window update left right +1 position
                     left = mpp.get(s.charAt(right)) + 1;
+
                 }
 
             }
-            // if the current string window or substring length is greater than len updating
-            // it
+            // if the current string window or substring length is greater than len updating it.
+         
             len = Math.max(len, right - left + 1);
             // and storing rightTH letter of string at rightTH position in hashmap
             mpp.put(s.charAt(right), right);
