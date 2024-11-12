911369f40758f67953dee61cf692fbadb828397f
Teja <88682769+lammu21@users.noreply.github.com>
2024-11-12 07:03:28 +0530

    Time: 11 ms (43.64%), Space: 48.9 MB (11.13%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,21 +1,17 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
         int n=g.length;
         int m=s.length;
         int l=0,r=0;
-
-        Arrays.sort(g);
-        Arrays.sort(s);
-
-        while(l<n&&r<m){
-            if(g[l]<=s[r]){
+        while(l<m&&r<n){
+            if(g[r]<=s[l]){
                 r=r+1;
             }
             l=l+1;
         }
-       
- 
-    return r;
-
+        return r;
+        
     }
 }
