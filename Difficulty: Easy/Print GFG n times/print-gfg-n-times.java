45355bd842769e2d2d38616953246ba3adf19b7c
Teja <88682769+lammu21@users.noreply.github.com>
2024-11-22 07:39:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print GFG n times/print-gfg-n-times.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution obj = new Solution();
+            obj.printGfg(n);
+            System.out.println();
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    
+    int count=0;
+
+    void printGfg(int N) {
+       int count=N;
+        // code here
+        if(count==0){
+            return;
+        }
+        System.out.print("GFG"+""+" ");
+        count--;
+        printGfg(count);
+    }
+}
