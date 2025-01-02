c47891f95567b741dca501ddc31276dd5265ffde
saiTeja <88682769+lammu21@users.noreply.github.com>
2025-01-02 07:13:48 +0530

    Time: 0 ms (100%), Space: 40 MB (90.28%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -11,24 +11,45 @@ class Solution {
         
     //     return climbStairs(n-1)+climbStairs(n-2);
     // }
-
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------
 // Recursion With memoization
-      public int climbStairs(int n) {
-        HashMap<Integer, Integer> memo = new HashMap<>();
-        return climbStairsHelper(n,memo);
+    //   public int climbStairs(int n) {
+    //     HashMap<Integer, Integer> memo = new HashMap<>();
+    //     return climbStairsMemo(n,memo);
 
-      }
-      public int climbStairsHelper(int n,HashMap<Integer,Integer> memo){
-          if(n<=1){
-            return 1;
-          }
-          if(memo.containsKey(n)){
-            return memo.get(n);
-          }
+    //   }
+    //   public int climbStairsMemo(int n,HashMap<Integer,Integer> memo){
+    //       if(n<=1){
+    //         return 1;
+    //       }
+    //       if(memo.containsKey(n)){
+    //         return memo.get(n);
+    //       }
 
-          int result = climbStairsHelper(n-1,memo)+climbStairsHelper(n-2,memo);
-          memo.put(n,result);
+    //       int result = climbStairsMemo(n-1,memo)+climbStairsMemo(n-2,memo);
+    //       memo.put(n,result);
           
-          return result;
-      }
+    //       return result;
+    //   }
+
+
+//  ------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Tabulation Approach
+   public int climbStairs(int n) {
+      return climbStairTab(n);
+   }
+
+  public int climbStairTab(int n){
+
+    if (n <= 1) return 1;
+    int [] dp= new int [n+1];
+     dp[0]=1;
+     dp[1]=1;
+    for(int i =2;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+    }
+    return dp[n];
+   }
 }
+
+
