dd36750ed5d19956d0e8dc64ca196c2fd120619b
saiTeja <88682769+lammu21@users.noreply.github.com>
2025-01-08 07:10:59 +0530

    Time: 0 ms (100%), Space: 40.8 MB (88.7%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -3,8 +3,26 @@ class Solution {
         int n= nums.length;
         int [] memo= new int [n];
         Arrays.fill(memo,-1);
-        return robMemo(n-1,nums,memo);
+        // return robMemo(n-1,nums,memo);
+        int [] tab= new int [n];
+        return robTab(n,nums,tab);
     }
+    static int robTab(int n,int []nums,int[] dp){
+        dp[0]=nums[0];
+        // int negative =0;
+        for(int i =1;i<n;i++){
+            int pick=nums[i];
+            if(i>1){
+                pick+=dp[i-2];
+            }
+            int notPick=dp[i-1];
+
+            dp[i]=Math.max(pick,notPick);
+        }
+        return dp[n-1];
+            
+        }
+    
     static int robMemo(int n,int []nums,int [] memo){
         if(n==0){
             return nums[0];
