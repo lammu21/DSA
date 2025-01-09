e341f26c22667bed049df78cfc5184ad2ff90aab
saiTeja <88682769+lammu21@users.noreply.github.com>
2025-01-09 06:48:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Geek Jump/geek-jump.java
+++ Difficulty: Easy/Geek Jump/geek-jump.java
@@ -40,23 +40,48 @@ class Solution{
         int [] memo= new int [N+1];
         Arrays.fill(memo,-1);
         // Arrays.fill(dp,-1);
-        return minimumEnergyhelper(arr,N-1,memo);
-    }
-    public static int minimumEnergyhelper(int[] arr, int N,int[] memo){
-        if(N==0){
-            return 0;
-        }
-        if(memo[N]!=-1){
-            return memo[N];
-        }
-        
-        int singlejump=minimumEnergyhelper(arr,N-1,memo)+Math.abs(arr[N]-arr[N-1]);
-        
-        int doublejump=Integer.MAX_VALUE;
-        if(N>1){
-             doublejump=minimumEnergyhelper(arr,N-2,memo)+ Math.abs(arr[N]-arr[N-2]);
-        }
-        return memo[N]= Math.min(singlejump,doublejump);
+        // return minimumEnergyhelper(arr,N-1,memo);
+        return minimumEnergyhelper(arr,N,memo);
         
     }
+    // public static int minimumEnergyhelper(int[] arr, int N,int[] memo){
+    //     if(N==0){
+    //         return 0;
+    //     }
+    //     if(memo[N]!=-1){
+    //         return memo[N];
+    //     }
+        
+    //     int singlejump=minimumEnergyhelper(arr,N-1,memo)+Math.abs(arr[N]-arr[N-1]);
+        
+    //     int doublejump=Integer.MAX_VALUE;
+    //     if(N>1){
+    //          doublejump=minimumEnergyhelper(arr,N-2,memo)+ Math.abs(arr[N]-arr[N-2]);
+    //     }
+    //     return memo[N]= Math.min(singlejump,doublejump);
+        
+    // }
+    
+      public static int minimumEnergyhelper(int[] arr, int n,int[] tab){
+          if(n==0) return 0;
+        //   if(tab[n]!=-1) return tab[n];
+        tab[0]=0;
+          
+          for(int i=1;i<n;i++){
+          
+          int doubleJump=Integer.MAX_VALUE;
+          
+          
+          int singleJump=tab[i-1]+Math.abs(arr[i]-arr[i-1]);
+          
+          if(i>1){
+              doubleJump=tab[i-2]+Math.abs(arr[i]-arr[i-2]);
+          }
+          
+          tab[i]=Math.min(singleJump,doubleJump);
+          
+      }
+      return tab[n-1];
+    
     }
+}
