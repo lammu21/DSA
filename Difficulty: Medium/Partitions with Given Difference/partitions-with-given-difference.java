4e87108c26d7612f250a521513317b3234c82720
saiTeja <88682769+lammu21@users.noreply.github.com>
2025-02-28 07:22:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.java
+++ Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.java
@@ -31,7 +31,8 @@ class GFG {
 
 
 class Solution {
-    int countPartitions(int[] arr, int d) {
+    
+     int countPartitions(int[] arr, int d) {
         // code here
         int totSum=0;
         int n=arr.length;
@@ -54,37 +55,98 @@ class Solution {
         
         int [][] dp= new int [n][target+1];
         
-        for(int [] row :dp){
-            Arrays.fill(row,-1);
+        // if(arr[0]==0&&target==0)
+        //     dp[0][0]= 2;
+       
+            dp[0][0]= 1;
+        
+        if(arr[0]<=target){
+            dp[0][arr[0]]+=1;
+            
+            
         }
         
-        return func(n-1,target,arr,dp);
+        for(int ind=1;ind<n;ind++){
+            for(int tar=0;tar<=target;tar++){
+                int notTaken=dp[ind-1][tar];
+                
+                int taken=0;
+                if(arr[ind]<=tar){
+                    taken =dp[ind-1][tar-arr[ind]];
+                }
+                dp[ind][tar]=taken+notTaken;
+            }
+        }
+        
+        
+        return dp[n-1][target];
+     }
 }
- int func(int ind,int target,int [] arr,int [][]dp){
-     
-     if(ind==0){
-         if(target==0&& arr[0]==0) return 2;
-         
-         if(target==0||arr[0]==target) return 1;
-         
-         return 0;
-         
-     }
-     
-     
-     if(dp[ind][target]!=-1){
-         return dp[ind][target];
-     }
-     
-     int notTaken= func(ind-1,target,arr,dp);
-     
-     int taken=0;
-     if(arr[ind]<=target){
-         taken=func(ind-1,target-arr[ind],arr,dp);
-     }
-     
-     return dp[ind][target]=taken+notTaken;
-     
- }
+    
+    
+    
+    
+// int countPartitions(int[] arr, int d) {
+//         // code here
+//         int totSum=0;
+//         int n=arr.length;
+        
+//         for(int i=0;i<n;i++){
+            
+//             totSum+=arr[i];
+            
+//         }
+        
+//         if((totSum-d)%2!=0 ||totSum<d){
+//             return 0;
+//         }
+        
+//         int target= (totSum-d)/2;
+        
+//         if(target<0){
+//             return 0;
+//         }
+        
+//         int [][] dp= new int [n][target+1];
+        
+//         for(int [] row :dp){
+//             Arrays.fill(row,-1);
+//         }
+        
+//         return func(n-1,target,arr,dp);
+// }
 
-}
+
+
+
+
+
+
+//  int func(int ind,int target,int [] arr,int [][]dp){
+     
+//      if(ind==0){
+//          if(target==0&& arr[0]==0) return 2;
+         
+//          if(target==0||arr[0]==target) return 1;
+         
+//          return 0;
+         
+//      }
+     
+     
+//      if(dp[ind][target]!=-1){
+//          return dp[ind][target];
+//      }
+     
+//      int notTaken= func(ind-1,target,arr,dp);
+     
+//      int taken=0;
+//      if(arr[ind]<=target){
+//          taken=func(ind-1,target-arr[ind],arr,dp);
+//      }
+     
+//      return dp[ind][target]=taken+notTaken;
+     
+//  }
+
+// }
