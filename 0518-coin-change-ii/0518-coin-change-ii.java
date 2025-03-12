706275fb531959f9c4473f25f553817ea6525f64
saiTeja <88682769+lammu21@users.noreply.github.com>
2025-03-12 06:56:35 +0530

    Time: 9 ms (63.28%), Space: 49.8 MB (16.07%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -2,34 +2,25 @@ class Solution {
     public int change(int amount, int[] coins) {
         int n=coins.length;
         int [][] dp = new int [n][amount+1];
-        for(int row[] : dp){
-            Arrays.fill(row,-1);
+        for(int tar=0;tar<=amount;tar++){
+            if(tar%coins[0]==0){
+                dp[0][tar]=1;
+            }
         }
+        for(int ind=1;ind<n;ind++){
+            for(int tar=0;tar<=amount;tar++){
+                int notTaken=dp[ind-1][tar];
 
-        return f(amount,coins,n-1,dp);
+
+                int taken=0;
+                if(coins[ind]<=tar){
+                    taken=dp[ind][tar-coins[ind]];
+                }
+                dp[ind][tar]=taken+notTaken;
+            }
+
+        }
+        return dp[n-1][amount];
         
-    }
-    public int f(int amount, int [] coins,int ind,int[][] dp){
-
-        if(ind==0){
-            if(amount%coins[0]==0){
-               return 1;
-            }
-            else {
-                return 0;
-            }
-        }
-        if(dp[ind][amount]!=-1){
-            return dp[ind][amount];
-        }
-
-        int notTaken=f(amount,coins,ind-1,dp);
-        int taken=0;
-
-        if(coins[ind]<=amount){
-            taken=f(amount-coins[ind],coins,ind,dp);
-        }
-        return dp[ind][amount] = notTaken+taken;
-
     }
 }
