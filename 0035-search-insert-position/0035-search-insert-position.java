6fb5a9bda2d5268d296673bb67f898bfb13d732f
saiTeja <88682769+lammu21@users.noreply.github.com>
2025-07-12 15:51:25 +0530

    Time: 0 ms (100%), Space: 42.8 MB (92.49%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,29 +1,36 @@
 class Solution {
 
-
+// Upper bound modification 
+// Check comments to understand difference 
     public int searchInsert(int[] nums, int target) {
-        int n=nums.length;
+         int len=nums.length;
+        int start=0;
+        int end=len-1;
 
-        int start=0,end=n-1,ans=-1;
+// to store the return value 
 
+        int result=-1;
         while(start<=end){
-            int mid =  (start+end)/2;
-
+            int mid=start+(end-start)/2;
+//   if target is equal, we just return mid 
             if(nums[mid]==target){
-                return mid;
+              return mid;
+        }
+// if target is greater than mid value, we save mid if number not present we need return floor(before) of the number 
+            else if (nums[mid]<target){
+                 result=mid;
+                 start=mid+1;
             }
-            else if(nums[mid]<target){
-                  ans=mid;
-                  start=mid+1;
 
-            }else{
+            else{
                 end=mid-1;
             }
-
-      
             
-            }
-                  return ans+1;
-        
+        }
+// returning floor +1 index, so position will be insert of target in array, 
+        //  return result+1;
+            return start;
+// We can alse return start, start will be at insert position or floor position of number 
+    
     }
 }
