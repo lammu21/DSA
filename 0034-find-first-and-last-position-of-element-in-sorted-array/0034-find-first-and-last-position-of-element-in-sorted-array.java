0f28982e2f3b4c80d38da0d32296d1f8b3950200
saiTeja <88682769+lammu21@users.noreply.github.com>
2025-07-28 10:19:14 +0530

    Time: 0 ms (100%), Space: 46.3 MB (8.6%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,69 +1,73 @@
 class Solution {
-// Regular binary search modified to search first occurence
-// Read comments to  understand changes 
-public int first(int[] nums, int target) {
-        int len=nums.length;
-        int start=0;
-        int end=len-1;
-// variable to keep track of First position of Element in Sorted array 
-        int first=-1;
-        while(start<=end){
-            int mid=(start+end)/2;
-            if(nums[mid]==target){
-// If the element is = target we save in First variable and, we discard second or right half search of array ,i.e we are looking for first occurence that will be in left present as it is a sorted 
-                first=mid;
-                end=mid-1;
+        public int[] searchRange(int[] nums, int target)
+        {
+            int n= nums.length;
+            int start=0, end=n-1;
+
+            int first =-1,last=-1;
+
+            // int [] arr={-1,-1};
+
+            first = findFirst(start,end,nums,target);
+        // if We Find First i.e., target present in that array,
+        //  then we Search For Last occurence of that Number..
+            if(first == -1)
+            {
+            
+                return new int[] {-1,-1};
             }
-            else if(nums[mid]>target){
-                end=mid-1;
-            }
-            else {
-                start=mid+1;
-            }
+            
+            last =findLast(start,end,nums,target);
+
+            return new int []{first,last};
+            
         }
-        return first;
-    }
+        public static int findFirst(int start, int end , int[] nums, int target)
+        {
 
-// Regular binary search modified to search Last occurence
-// Read comments to  understand changes 
+            int first = -1;
+            while(start<=end)
+            {
+                int mid = (start+end)/2;
 
-    public int last(int[] nums, int target) {
-        int len=nums.length;
-        int start=0;
-        int end=len-1;
-// variable to keep track of Last position of Element in Sorted array 
-        int last=-1;
-        while(start<=end){
-            int mid=(start+end)/2;
-
-// If the element is = target we save in last variable and , we discard first or left half ,i.e we are looking for last occurence that will be in present right half  as it is a sorted 
-            if(nums[mid]==target){
-                last=mid;
-                start=mid+1;
+                if(nums[mid] == target)
+                {
+                    first = mid;
+                    end = mid-1;
+                }
+                else if (nums[mid] > target)
+                {
+                    end = mid-1;
+                }
+                else
+                {
+                    start = mid+1;
+                }
             }
-            else if(nums[mid]>target){
-                end=mid-1;
-            }
-            else {
-                start=mid+1;
-            }
+            return first;
         }
-        return last;
-    }
+        public static int findLast(int start, int end , int[] nums, int target)
+        {
 
-// Leetcode function which calls first and second position's binary search
+            int last = -1;
+            while(start<=end)
+            {
+                int mid = (start+end)/2;
 
-    public int[] searchRange(int[] nums, int target) {
-
-        int first=first(nums,target);
-// if we dont find occurrence of number, i.e., array does've that number we can directly {-1,-1}
-        if(first==-1){
-             int [] result={-1,-1};
-            return result;
+                if(nums[mid] == target)
+                {
+                    last = mid;
+                    start = mid+1;
+                }
+                else if (nums[mid] > target)
+                {
+                    end = mid-1;
+                }
+                else
+                {
+                    start = mid+1;
+                }
+            }
+            return last;
         }
-        int last=last(nums,target);
-        int [] result={first,last};
-
-        return result;
-    }
 }
